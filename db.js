cdf8447cc7b3a15a2963ce29f823ed359a9ae0e1
frostie <mattwiner04@gmail.com>
2025-05-21 14:15:44 -0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,31 @@
+import postgres from 'postgres'
+import dotenv from "dotenv";
+dotenv.config();
+const connectionString = process.env.POSTGRES_URL;
+const sql = postgres(connectionString);
+async function setupDB() {
+    console.log("Database Connected");
+    console.log("Starting DB...");
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                password_hash TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )`;
+        await sql`
+            CREATE TABLE IF NOT EXISTS blogPosts (
+                id SERIAL PRIMARY KEY,
+                postTitle VARCHAR(50) UNIQUE NOT NULL,
+                postSubtitle VARCHAR(50) UNIQUE NOT NULL,
+                postText VARCHAR(150) UNIQUE NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )`;
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+export {sql,setupDB};
