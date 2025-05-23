79f6ba3150bb915f16b987de6a3c8dc99f626aa4
frostie <mattwiner04@gmail.com>
2025-05-23 10:04:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const app = express();
 const VIEWS_DIR = path.join(__dirname, "views");
 const PARTIALS_DIR = path.join(VIEWS_DIR, "partials");
 const PUBLIC_DIR = path.join(__dirname, "public");
-
 const upload = multer();
 const PORT = process.env.PORT || 3003;
 
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/styles", express.static(path.join(__dirname, "public/styles")));
+app.use("/scripts", express.static(path.join(__dirname, "public/scripts")));
 // Template engine
 app.engine("html", engine({ extname: ".html", defaultLayout: false, partialsDir: PARTIALS_DIR }));
 app.set("view engine", "html");
